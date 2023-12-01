3ec10431dafbbeb7dcdcfa490c920c0bd136bfaf
xavierroy12 <201551593@cegepsherbrooke.qc.ca>
2023-12-01 16:19:28 -0500

    Add project files.

---
+++ BlazorToDo/BlazorToDo/Classes/Category.cs
@@ -0,0 +1,31 @@
+﻿using BlazorToDo.Components;
+
+namespace BlazorToDo.Classes
+{
+    public class Category
+    {
+        public static Category Default { get; } = new Category(1, "");
+        public static Category DefaultInternal { get; set; } = Default;
+        public int Id { get; set; }
+        public string name { get; set; }
+        public Category(int id, string name)
+        {
+            this.Id = id;
+            this.name = name;
+        }
+        public void clear()
+        {
+            Id = Default.Id;
+            name = Default.name;
+        }
+        public void clearInternal()
+        {
+            Id = DefaultInternal.Id;
+            name = DefaultInternal.name;
+        }
+        public Category copy() 
+        { 
+            return new Category(this.Id, this.name);
+        }
+    }
+}
---
+++ BlazorToDo/BlazorToDo/Classes/ToDoItem.cs
@@ -0,0 +1,38 @@
+﻿namespace BlazorToDo.Classes
+{
+    public class ToDoItem
+    {
+        public static ToDoItem Default { get; } = new ToDoItem(0, "", Category.Default);
+        public int toDoID { get; set; }
+        public string toDo { get; set; }
+        public Category category { get; set; }
+        public ToDoItem(int toDoID, string toDo, Category category)
+        {
+            this.toDoID = toDoID;
+            this.toDo = toDo;
+            this.category = category;
+        }
+        public ToDoItem(int toDoID, string toDo, int categoryID, string category)
+        {
+            this.toDoID = toDoID;
+            this.toDo = toDo;
+            this.category = new Category(categoryID, category);
+        }
+        public void clear()
+        {
+            toDoID = Default.toDoID;
+            category.clear();
+            toDo = Default.toDo;
+        }
+        public void clearInternal()
+        {
+            toDoID = Default.toDoID;
+            category.clearInternal();
+            toDo = Default.toDo;
+        }
+        public ToDoItem copy()
+        {
+            return new ToDoItem(this.toDoID, this.toDo, this.category.copy());
+        }
+    }
+}
